dd5f2916bb63f74bbafd22d50f2d62e97f258681
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-03-29 16:03:38 +0100

    exA Q3 complete

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -5,6 +5,7 @@ stops.append("Edinburgh Waverly")
 #2. Add "Glasgow Queen St" to the start of the list
 stops.insert(0, "Gloasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(3, "Polmont")
 #4. Print out the index position of "Linlithgow"
 #5. Remove "Livingston" from the list using its name
 #6. Delete "Cumbernauld" from the list by index
