34c20f9cacd8aa2bdbee164f3e7ea63edf9882ad
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-03-29 17:24:28 +0100

    eB Q7 done

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -72,7 +72,8 @@ for numbers in avrils_lottery_numbers:
     even_numbers.append(numbers)
   print(even_numbers)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-
+users ["Erik"] ["lottery_numbers"].append(7)
+print(users["Erik"] ["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
