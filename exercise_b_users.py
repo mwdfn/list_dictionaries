6989f30eace9576c6254180407a843eabb918164
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-03-29 17:26:09 +0100

    eB Q8 done

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -75,5 +75,7 @@ for numbers in avrils_lottery_numbers:
 users ["Erik"] ["lottery_numbers"].append(7)
 print(users["Erik"] ["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
+users ["Erik"] ["home_town"] = "Edinbrugh"
+print(users["Erik"] ["home_town"])
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
